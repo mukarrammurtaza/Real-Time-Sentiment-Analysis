a85f75ef4aa48c4a750408d89e1c11d0689bd1da
Mukarram Murtaza <98780798+mukarrammurtaza@users.noreply.github.com>
2023-12-03 23:21:32 +0500

    minor changes

--- main.py
+++ main.py
@@ -6,12 +6,11 @@ from kafka import KafkaProducer
 producer = KafkaProducer(bootstrap_servers='localhost:9092',
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 reddit = praw.Reddit(
-    client_id='2jut2DJLXe3RrpEG_FNyJQ',  # Replace with your client id
-    client_secret='LnAtNcNbSMRIXN0WKcRmSgGXOzfVTg',  # Replace with your client secret
-    user_agent='SentimentalAnalyser',  # Name of your application
-    username='SentimentalAnalyser1',  # Your Reddit username
-    # password='abbasigang@61'  # Your Reddit password
-)
+    client_id='',  # Replace with your client id
+    client_secret='',  # Replace with your client secret
+    user_agent='',  # Name of your application
+    username='',  # Your Reddit username
+   
 
 def get_comments(submission):
     submission.comments.replace_more(limit=0)
